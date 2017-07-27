c4506f64ec87480818697b2a1ccf5bf5c79e8922
Nilay Mehta <nilay73@gatech.edu>
2017-07-27 16:18:52 -0400

    Optimization for same length sufixes

--- src/main/java/com/wordsearch/DAWG/DawgDS.java
+++ src/main/java/com/wordsearch/DAWG/DawgDS.java
@@ -77,19 +77,31 @@ public class DawgDS implements Serializable{
             System.out.println("Optimizing for index " + i);
             System.out.println("**********************************************************************");
 
-            for(int j = 0; j < nodeEndings.size() - 1; j++) {
+            boolean breakOne = false;
+            boolean cont = true;
+            int j = 0;
+            ArrayList<Integer> indexBlacklist = new ArrayList<>();
+            System.out.println(nodeEndings.size() - 1);
+            while(j < nodeEndings.size() - 1) {
                 Node one = nodeEndings.get(j);
-                for (int l = j + 1; l < nodeEndings.size(); l++) {
+                cont = true;
+                int l = j + 1;
+                while(cont && l < nodeEndings.size()) {
                     Node two = nodeEndings.get(l);
-                    if(one.equals(two)) {
+                    if(!indexBlacklist.contains(l) && one.equals(two)) {
                         Node oneParent = one.getParent();
                         boolean removed = oneParent.removeChildNode(one.getValue());
                         oneParent.getChildren().add(two);
                         nodeCount--;
+                        indexBlacklist.add(l);
                         totalRemoved++;
                         System.out.println("Running total Nodes Removed Count: " + totalRemoved);
+                        System.out.println("Removed for letter : " + two.getValue() + " and for the parent letters for one and two of: " + one.getParent().getValue() + ", " + two.getParent().getValue());
+                        cont = false;
                     }
+                    l++;
                 }
+                j++;
             }
         }
         System.out.println("**********************************************************************");
